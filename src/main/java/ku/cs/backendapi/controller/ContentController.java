fb5c778923fcc3293cfa2087ca9ebe1a4cd2dfaf
mawmeaww <nonokub123@hotmail.com>
2023-10-16 17:02:36 +0700

    get all category api

--- src/main/java/ku/cs/backendapi/controller/ContentController.java
+++ src/main/java/ku/cs/backendapi/controller/ContentController.java
@@ -1,11 +1,13 @@
 package ku.cs.backendapi.controller;
 
+import ku.cs.backendapi.entity.Category;
 import ku.cs.backendapi.exception.TokenNotfoundException;
 import ku.cs.backendapi.exception.UserNotFoundException;
 import ku.cs.backendapi.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -18,5 +20,10 @@ public class ContentController {
     @GetMapping("/image")
     public String getUserImage(@RequestParam String token) throws UserNotFoundException, TokenNotfoundException {
         return service.getImageLinkCustomer(UUID.fromString(token));
+    }
+
+    @GetMapping("/category")
+    public List<String> getAllCategory() {
+        return service.getAllCategory();
     }
 }
--- src/main/java/ku/cs/backendapi/service/ContentService.java
+++ src/main/java/ku/cs/backendapi/service/ContentService.java
@@ -1,12 +1,17 @@
 package ku.cs.backendapi.service;
 
+import ku.cs.backendapi.entity.Category;
 import ku.cs.backendapi.entity.User;
 import ku.cs.backendapi.exception.TokenNotfoundException;
 import ku.cs.backendapi.exception.UserNotFoundException;
+import ku.cs.backendapi.repository.CategoryRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ContentService {
@@ -16,9 +21,22 @@ public class ContentService {
     @Autowired
     TokenService tokenService;
 
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
     public String getImageLinkCustomer(UUID tokenId) throws UserNotFoundException, TokenNotfoundException {
         User user = tokenService.getUser(tokenId);
         if(user.getImageLink() == null) return host + "/shibaqueue/userImage/profile.png";
         return user.getImageLink();
+    }
+
+    public List<String> getAllCategory() {
+        List<Category> categoryList = categoryRepository.findAll();
+        return categoryList.stream()
+                .map(category -> modelMapper.map(category, String.class))
+                .toList();
     }
 }
